45db4ffeadddca5a247becf1f42bcc9dd42ecfa6
BayuuPermana <bayu1234p@gmail.com>
2023-11-17 09:56:47 +0700

    Menambahkan button untuk menampilkan isi cerita

--- lib/main.dart
+++ lib/main.dart
@@ -12,51 +12,158 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Web Scraping'),
+          title: Text('Web Scraping with Flutter'),
         ),
-        body: FutureBuilder(
-          future: fetchData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(snapshot.data![index]),
-                  );
-                },
-              );
-            }
-          },
-        ),
+        body: WebScrapingWidget(),
       ),
     );
   }
+}
 
-  Future<List<String>> fetchData() async {
+class WebScrapingWidget extends StatefulWidget {
+  @override
+  _WebScrapingWidgetState createState() => _WebScrapingWidgetState();
+}
+
+class _WebScrapingWidgetState extends State<WebScrapingWidget> {
+  List<String> titles = [];
+  String selectedTitle = '';
+  String selectedDetail = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    await fetchData();
+                  },
+                  child: Text('Fetch Data'),
+                ),
+                if (titles.isNotEmpty)
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: titles.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(titles[index]),
+                          onTap: () {
+                            setState(() {
+                              selectedTitle = titles[index];
+                              selectedDetail = ''; 
+                            });
+                          },
+                          selected: titles[index] == selectedTitle,
+                          tileColor: titles[index] == selectedTitle
+                              ? Colors.blue.withOpacity(0.2)
+                              : null,
+                        );
+                      },
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (selectedTitle.isNotEmpty)
+                  ElevatedButton(
+                    onPressed: () async {
+                      await fetchDetail(selectedTitle);
+                    },
+                    child: Text('Show Detail'),
+                  ),
+                if (selectedDetail.isNotEmpty)
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          selectedDetail,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> fetchData() async {
     final response = await http.get(Uri.parse('https://www.liputan6.com/hot/read/4487363/18-cerita-lucu-pendek-yang-bikin-ngakak-sukses-ngocok-perut?page=2'));
     if (response.statusCode == 200) {
-      return parseHtml(response.body);
+      setState(() {
+        titles = parseTitles(response.body);
+      });
     } else {
       throw Exception('Failed to load data');
     }
   }
 
-  List<String> parseHtml(String responseBody) {
+  List<String> parseTitles(String responseBody) {
     final document = htmlParser.parse(responseBody);
-    final List<String> titles = [];
+    final List<String> extractedTitles = [];
 
-
-    final judul = document.querySelectorAll('h2'); 
-    for (final element in judul) {
-      titles.add(element.text);
+    final elements = document.querySelectorAll('h2');
+    for (final element in elements) {
+      extractedTitles.add(element.text);
     }
 
-    return titles;
-    
+    return extractedTitles;
   }
+
+  Future<void> fetchDetail(String title) async {
+    final response = await http.get(Uri.parse('https://www.liputan6.com/hot/read/4487363/18-cerita-lucu-pendek-yang-bikin-ngakak-sukses-ngocok-perut?page=2'));
+    if (response.statusCode == 200) {
+      setState(() {
+        selectedDetail = parseDetail(response.body, title);
+      });
+    } else {
+      throw Exception('Failed to load detail');
+    }
+  }
+
+  // ...
+
+String parseDetail(String responseBody, String title) {
+  final document = htmlParser.parse(responseBody);
+  final elements = document.querySelectorAll('h2');
+
+  bool foundTitle = false;
+  StringBuffer detailBuffer = StringBuffer();
+
+  for (final element in elements) {
+    if (element.text == title) {
+      foundTitle = true;
+      var nextElements = element.nextElementSibling;
+      while (nextElements != null && nextElements.localName != 'h2') {
+        if (nextElements.localName == 'p') {
+          detailBuffer.write(nextElements.text);
+          detailBuffer.write('\n\n'); 
+        }
+        nextElements = nextElements.nextElementSibling;
+      }
+      break;
+    }
+  }
+
+  return foundTitle ? detailBuffer.toString() : 'Detail not found';
+}
+
+// ...
+
 }
